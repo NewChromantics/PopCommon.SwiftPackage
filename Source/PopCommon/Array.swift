388370a8f97c71a4fc501828578125ee8329490d
Graham Reeves <graham@grahamreeves.com>
2025-02-04 17:52:00 +0000

    Added .sum() to arrays for arrays of ints

--- Source/PopCommon/Array.swift
+++ Source/PopCommon/Array.swift
@@ -2,6 +2,9 @@
 	Array utilities
 */
 
+//	this can now be done with 
+//		import Algoritm
+//		let unique = Array( origArray.uniqued() )
 extension Array where Element: Hashable
 {
 	public func reduce() -> Set<Self.Element>
@@ -9,4 +12,14 @@ extension Array where Element: Hashable
 		return Set(self.map{$0})
 	}
 	
+}
+
+//	https://stackoverflow.com/a/49046981/355753
+extension Sequence where Element : Numeric 
+{
+	//	Returns the sum of all elements in the collection
+	public func sum() -> Element 
+	{
+		return reduce(0, +) 
+	}
 }
