dc4b0fe25d4ab411da5a545c8a9cc4910b598ab0
Graham Reeves <graham@grahamreeves.com>
2025-02-10 15:24:26 +0000

    Added Array.SplitArray to split an array into chunks of smaller arrays

--- Source/PopCommon/Array.swift
+++ Source/PopCommon/Array.swift
@@ -23,3 +23,26 @@ extension Sequence where Element : Numeric
 		return reduce(0, +) 
 	}
 }
+
+
+
+//	split an array into chunks
+extension Array
+{
+	public func splitArray(maxPerChunk:Int) -> [[Self.Element]]
+	{
+		let maxPerChunkf = Double(Swift.max(Int(1),maxPerChunk))
+		let chunksf = Double(self.count) / maxPerChunkf
+		let chunks = Swift.max( 1, ceil(chunksf) )
+		let elementsPerChunk = Int( ceil( Double(self.count) / chunks ) )
+		
+		let chunkedArray = stride(from: 0, to: self.count, by: elementsPerChunk).map 
+		{
+			firstIndex in
+			let lastIndex = Swift.min(firstIndex + elementsPerChunk, self.count)
+			return Array( self[firstIndex..<lastIndex] )
+		}
+		
+		return chunkedArray
+	}
+}
