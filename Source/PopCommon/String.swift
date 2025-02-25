267cabcf65a95ced2c9bd2780838b372470f38eb
Graham Reeves <graham@grahamreeves.com>
2025-02-25 11:44:53 +0000

    Added string sub-string accessors

--- Source/PopCommon/String.swift
+++ Source/PopCommon/String.swift
@@ -29,3 +29,19 @@ extension String
 		return String(prefix)
 	}
 }
+
+extension StringProtocol
+{
+	public subscript(offset: Int) -> Character { self[index(startIndex, offsetBy: offset)] }
+	public subscript(range: Range<Int>) -> SubSequence {
+		let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+		return self[startIndex..<index(startIndex, offsetBy: range.count)]
+	}
+	public subscript(range: ClosedRange<Int>) -> SubSequence {
+		let startIndex = index(self.startIndex, offsetBy: range.lowerBound)
+		return self[startIndex..<index(startIndex, offsetBy: range.count)]
+	}
+	public subscript(range: PartialRangeFrom<Int>) -> SubSequence { self[index(startIndex, offsetBy: range.lowerBound)...] }
+	public subscript(range: PartialRangeThrough<Int>) -> SubSequence { self[...index(startIndex, offsetBy: range.upperBound)] }
+	public subscript(range: PartialRangeUpTo<Int>) -> SubSequence { self[..<index(startIndex, offsetBy: range.upperBound)] }
+}
