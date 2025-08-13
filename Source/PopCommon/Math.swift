1822a753493ee3000bcc67f573d0bd963c7080e9
Graham Reeves <graham@grahamreeves.com>
2025-08-13 13:53:36 +0100

    Added Range() and Clamp() for floats

--- Source/PopCommon/Math.swift
+++ Source/PopCommon/Math.swift
@@ -10,6 +10,16 @@ public func range(_ min:CGFloat,_ max:CGFloat,value:CGFloat) -> CGFloat
 	return (value-min) / (max-min)
 }
 
+public func range(_ min:Float,_ max:Float,value:Float) -> Float
+{
+	return (value-min) / (max-min)
+}
+
+//	matching simd order!
+public func clamp(_ x:Float,min:Float,max:Float) -> Float
+{
+	return Swift.max( min, Swift.min( x, max ) )
+}
 
 public func lerp(_ min:CGFloat,_ max:CGFloat,_ time:CGFloat) -> CGFloat
 {
