6d6c9ba397add250c90832bba777e8618ac2b184
Graham Reeves <graham@grahamreeves.com>
2025-09-19 12:46:10 +0100

    .rgb to compliment .rgba on simd4

--- Source/PopCommon/Extensions/Color.swift
+++ Source/PopCommon/Extensions/Color.swift
@@ -9,6 +9,12 @@ public extension Color
 		let uic = UIColor(self)
 		return uic.cgColor.rgba
 	}
+	
+	var rgb : simd_float3?
+	{
+		let uic = UIColor(self)
+		return uic.cgColor.rgb
+	}
 }
 
 public extension CGColor
@@ -34,6 +40,26 @@ public extension CGColor
 		return simd_float4( r,g,b,a )
 	}
 	
+	var rgb : simd_float3?	
+	{
+		guard let components = self.components, components.count > 0 else 
+		{
+			return nil
+		}
+		
+		//	handle sub-3 component colours (monochrome)
+		let r = Float(components[0])
+		
+		if components.count < 3
+		{
+			return simd_float3( r, r, r )
+		}
+		
+		let g = Float(components[1])
+		let b = Float(components[2])
+		return simd_float3( r,g,b )
+	}
+	
 	static func fromRgba(_ rgba:simd_float4) -> CGColor
 	{
 		return CGColor(
@@ -41,6 +67,16 @@ public extension CGColor
 			green:	CGFloat(rgba.y),
 			blue:	CGFloat(rgba.z),
 			alpha:	CGFloat(rgba.w)
+		)
+	}
+	
+	static func fromRgb(_ rgb:simd_float3) -> CGColor
+	{
+		return CGColor(
+			red:	CGFloat(rgb.x),
+			green:	CGFloat(rgb.y),
+			blue:	CGFloat(rgb.z),
+			alpha:	CGFloat(1)
 		)
 	}
 }
